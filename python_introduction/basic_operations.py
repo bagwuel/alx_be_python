1ae27da8ce40fc5326af09bedbd55a3b7e2cda3e
bagwuel <mbagwu39@gmail.com>
2025-12-16 08:24:05 +0000

    python arithemetics

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,3 @@
+number1 = 10
+number2 = 5
+print(f"Addition of {number1} and {number2} is {number1 + number2}\nSubtraction of {number1} and {number2} is {number1 - number2}\nMultiplication of {number1} and {number2} is {number1 * number2}")
