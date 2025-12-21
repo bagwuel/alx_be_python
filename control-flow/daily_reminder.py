a08c24cb3a602be8724c9f5e0f900b7f974040eb
bagwuel <mbagwu39@gmail.com>
2025-12-21 11:05:21 +0000

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,16 +2,16 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-def message():
-     print(f"{task} is a {priority} priority task", end = "")
+def message(custom_msg):
+    print(f"{custom_msg}: {task} is a {priority} priority task", end = "")
 
 match priority:
     case "high":
-        message()
+        message("Reminder")
     case "medium":
-        message()
+        message("Alert")
     case "low":
-        message()
+        message("Note")
 if time_bound == "yes":
     print(" that requires immediate action")
 else:
