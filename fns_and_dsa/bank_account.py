7b0005f74e43d3186324bd60b3c51754daa1151a
bagwuel <mbagwu39@gmail.com>
2026-01-02 06:06:06 +0000

    bank account

---
+++ fns_and_dsa/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount():
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if (self.account_balance - amount) <= 0:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
